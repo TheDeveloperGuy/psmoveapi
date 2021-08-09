06b3eb14ecb126cc7e9b6d745cd4c8219817f9cb
TheDeveloperGuy <github@mediator-software.com>
2021-08-09 15:45:56 +0200

    Add PSMove bluetooth connect/disconnect functionality for Windows

--- src/psmoveapi.cpp
+++ src/psmoveapi.cpp
@@ -64,6 +64,10 @@ struct ControllerGlue {
     bool api_connected;
 };
 
+#ifdef _WIN32
+class DeviceNotificationWindow;
+#endif
+
 struct PSMoveAPI {
     PSMoveAPI(EventReceiver *receiver, void *user_data);
     ~PSMoveAPI();
@@ -75,12 +79,436 @@ struct PSMoveAPI {
     EventReceiver *receiver;
     void *user_data;
     std::vector<ControllerGlue *> controllers;
+#ifdef _WIN32
+	DeviceNotificationWindow *monitor;
+#else
     moved_monitor *monitor;
+#endif
 };
 
 PSMoveAPI *
 g_psmove_api = nullptr;
 
+#ifdef _WIN32
+# include <windows.h>
+# include <wrl/client.h>
+# include <dbt.h>
+# include <bluetoothapis.h>
+# include <hidapi.h>
+
+# ifdef _MSC_VER
+// Disable 64-bit portability warnings
+#  pragma warning(disable: 4312 )
+// Disable 'deprecated function' warnings
+#  pragma warning(disable: 4996)
+# endif // _MSC_VER
+
+#define NUM_PSMOVE_PIDS \
+    ((sizeof(PSMOVE_PIDS) / sizeof(PSMOVE_PIDS[0])) - 1)
+
+static int
+PSMOVE_PIDS[] = {
+	PSMOVE_PID,
+	PSMOVE_PS4_PID,
+	0,
+};
+
+class DeviceNotificationWindow
+{
+public:
+	DeviceNotificationWindow(void *context);
+	~DeviceNotificationWindow();
+
+protected:
+	/** Open/close window **/
+	bool open();
+	void close();
+
+	/** Wait until window is closed **/
+	void waitForClose();
+
+private:
+	/** Notification window message handler **/
+	static LRESULT CALLBACK wndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+	/** Device notification handler **/
+	void handleBluetoothConnect(BTH_ADDR bthAddress, bool connect);
+
+protected:
+	/** Thread entry point **/
+	static DWORD WINAPI wndThreadProc(void *arg);
+
+	/** Start thread **/
+	bool startThread();
+
+	/** Stop thread **/
+	bool stopThread();
+
+	/** Create/destroy window **/
+	bool createWindow();
+	void destroyWindow();
+
+	/** Post message to window **/
+	void postMessage(UINT uMsg, WPARAM wParam = 0, LPARAM lParam = 0)
+	{
+		if (window != nullptr)
+		{
+			PostMessage(window, uMsg, wParam, lParam);
+		}
+	}
+
+private:
+	/* User data */
+	void *user_data;
+
+	/* Window and windows message thread handles */
+	HWND window;
+	HANDLE thread;
+	HANDLE semaphore;
+
+	/* Device notification handle */
+	HANDLE radio;
+	HDEVNOTIFY notification;
+};
+
+/** Constructor/destructor **/
+DeviceNotificationWindow::DeviceNotificationWindow(void *context)
+	: user_data(context)
+	, window(NULL)
+	, thread(INVALID_HANDLE_VALUE)
+	, semaphore(INVALID_HANDLE_VALUE)
+	, radio(INVALID_HANDLE_VALUE)
+	, notification(NULL)
+{
+	open();
+}
+
+DeviceNotificationWindow::~DeviceNotificationWindow()
+{
+	close();
+}
+
+bool DeviceNotificationWindow::open()
+{
+	// Start window message thread
+	return startThread();
+}
+
+void DeviceNotificationWindow::close()
+{
+	stopThread();
+	destroyWindow();
+}
+
+/** Wait until window is closed **/
+void DeviceNotificationWindow::waitForClose()
+{
+	// Wait for thread to quit
+	WaitForSingleObject(thread, INFINITE);
+}
+
+/** Start thread **/
+bool DeviceNotificationWindow::startThread()
+{
+	// Not already started?
+	if (thread == INVALID_HANDLE_VALUE)
+	{
+		// Create window initialisation semaphore
+		semaphore = CreateSemaphore(NULL, 0, 1, NULL);
+
+		// Start thread
+		thread = CreateThread(NULL, 0, wndThreadProc, (void*)this, 0, NULL);
+		if (thread != INVALID_HANDLE_VALUE)
+		{
+			// Wait for initialisation to complete
+			WaitForSingleObject(semaphore, 3000);
+			CloseHandle(semaphore);
+			semaphore = INVALID_HANDLE_VALUE;
+		}
+	}
+
+	return true;
+}
+
+/** Stop thread **/
+bool DeviceNotificationWindow::stopThread()
+{
+	// Thread running?
+	if (thread != INVALID_HANDLE_VALUE)
+	{
+		// Send quit message to thread
+		postMessage(WM_QUIT);
+		waitForClose();
+	}
+
+	return true;
+}
+
+/** Thread entry point **/
+DWORD WINAPI DeviceNotificationWindow::wndThreadProc(void *arg)
+{
+	DeviceNotificationWindow *thiz = reinterpret_cast<DeviceNotificationWindow*>(arg);
+	if (thiz != nullptr)
+	{
+		// Create window
+		if (thiz->createWindow())
+		{
+			// Get handle to first bluetooth radio 
+			BLUETOOTH_FIND_RADIO_PARAMS radio_params;
+			radio_params.dwSize = sizeof(BLUETOOTH_FIND_RADIO_PARAMS);
+			HBLUETOOTH_RADIO_FIND hFind = BluetoothFindFirstRadio(&radio_params, &thiz->radio);
+			if (hFind != NULL)
+			{
+				// Close find
+				BluetoothFindRadioClose(hFind);
+
+				// Register for device notifications
+				DEV_BROADCAST_HANDLE NotificationFilter;
+				ZeroMemory(&NotificationFilter, sizeof(NotificationFilter));
+				NotificationFilter.dbch_size = sizeof(DEV_BROADCAST_DEVICEINTERFACE);
+				NotificationFilter.dbch_devicetype = DBT_DEVTYP_HANDLE;
+				NotificationFilter.dbch_handle = thiz->radio;
+
+				thiz->notification = RegisterDeviceNotification(
+					thiz->window,     // events recipient
+					&NotificationFilter,        // type of device
+					DEVICE_NOTIFY_WINDOW_HANDLE // type of recipient handle
+				);
+
+				if (thiz->notification != NULL)
+				{
+					// Signal that thread has initialised
+					if (thiz->semaphore != INVALID_HANDLE_VALUE)
+					{
+						ReleaseSemaphore(thiz->semaphore, 1, NULL);
+					}
+
+					// Pump messages
+					MSG message;
+					while (GetMessage(&message, NULL, 0, 0))
+					{
+						// Process message
+						TranslateMessage(&message);
+						DispatchMessage(&message);
+					}
+
+					// Unregister for device notifications
+					UnregisterDeviceNotification(thiz->notification);
+					thiz->notification = NULL;
+				}
+
+				// Close bluetooth radio handle
+				CloseHandle(thiz->radio);
+				thiz->radio = INVALID_HANDLE_VALUE;
+			}
+
+			// Clear class pointer in window data
+			SetWindowLongPtr(thiz->window, GWLP_USERDATA, (LONG_PTR)0);
+
+			// Destroy window
+			thiz->destroyWindow();
+		}
+	}
+	else
+	{
+		// Signal that thread has failed to initialise
+		if (thiz->semaphore != INVALID_HANDLE_VALUE)
+		{
+			ReleaseSemaphore(thiz->semaphore, 1, NULL);
+		}
+	}
+
+	thiz->thread = INVALID_HANDLE_VALUE;
+
+	return 0;
+}
+
+/** Create output window **/
+bool DeviceNotificationWindow::createWindow()
+{
+	if (window == nullptr)
+	{
+		WNDCLASS windowClass;
+
+		// Set up window class structure
+		memset(&windowClass, 0, sizeof(WNDCLASS));
+		windowClass.style = CS_HREDRAW | CS_VREDRAW;
+		windowClass.lpfnWndProc = wndProc;
+		windowClass.hInstance = GetModuleHandle(NULL);
+		windowClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 2);
+		windowClass.lpszClassName = TEXT("PsMoveDevNotifyWnd");
+
+		// Register window class
+		if (!RegisterClass(&windowClass))
+		{
+			// Not already registered?
+			if (GetLastError() != ERROR_CLASS_ALREADY_EXISTS)
+			{
+				return false;
+			}
+		}
+
+		// Create window
+		if ((window = CreateWindowEx(0,
+			windowClass.lpszClassName, TEXT(""), 0,
+			0, 0, 0, 0, NULL, NULL, windowClass.hInstance, NULL)) == nullptr)
+		{
+			return false;
+		}
+
+		// Save class pointer in window data
+		SetWindowLongPtr(window, GWLP_USERDATA, (LONG_PTR)this);
+
+		// Show window
+		ShowWindow(window, SW_HIDE);
+		UpdateWindow(window);
+
+		// Create message queue
+		MSG message;
+		PeekMessage(&message, NULL, 0, 0, 0);
+	}
+
+	return true;
+}
+
+/** Destroy window **/
+void DeviceNotificationWindow::destroyWindow()
+{
+	if (window != NULL)
+	{
+		// Destroy window
+		DestroyWindow(window);
+		window = NULL;
+	}
+}
+
+static int
+compare_hid_device_info_ptr(const void *a, const void *b)
+{
+	struct hid_device_info *dev_a = *(struct hid_device_info **)a;
+	struct hid_device_info *dev_b = *(struct hid_device_info **)b;
+
+	if ((dev_a->serial_number != NULL) && (wcslen(dev_a->serial_number) != 0) &&
+		(dev_b->serial_number != NULL) && (wcslen(dev_b->serial_number) != 0)) {
+		return wcscmp(dev_a->serial_number, dev_b->serial_number);
+	}
+
+	if (dev_a->path != NULL && dev_b->path != NULL) {
+		return strcmp(dev_a->path, dev_b->path);
+	}
+	return 0;
+}
+
+/** Device notification handler **/
+void DeviceNotificationWindow::handleBluetoothConnect(BTH_ADDR bthAddress, bool connect)
+{
+	wchar_t serial_number[13];
+	unsigned char *address = (unsigned char*)&bthAddress;
+	_swprintf(serial_number, L"%02x%02x%02x%02x%02x%02x", address[5], address[4], address[3], address[2], address[1], address[0]);
+
+	if (connect) {
+		// Give HID device time to register
+		Sleep(500);
+	}
+
+	/* From psmove.c */
+	struct hid_device_info *move_hid_devices[NUM_PSMOVE_PIDS], *devs, *cur_dev;
+
+	// enumerate matching HID devices
+	for (unsigned int i = 0; i < NUM_PSMOVE_PIDS; i++) {
+		// NOTE: hidapi returns NULL for PIDs that were not found
+		move_hid_devices[i] = hid_enumerate(PSMOVE_VID, PSMOVE_PIDS[i]);
+	}
+
+	// Count available devices
+	int available = 0;
+	int i;
+	for (i = 0; i < NUM_PSMOVE_PIDS; i++) {
+		for (cur_dev = move_hid_devices[i]; cur_dev != NULL; cur_dev = cur_dev->next, available++);
+	}
+
+	// Sort list of devices to have stable ordering of devices
+	int n = 0;
+	struct hid_device_info **devs_sorted = (struct hid_device_info **) calloc(available, sizeof(struct hid_device_info *));
+	for (i = 0; i < NUM_PSMOVE_PIDS; i++) {
+		cur_dev = move_hid_devices[i];
+		while (cur_dev && (n < available)) {
+			devs_sorted[n] = cur_dev;
+			cur_dev = cur_dev->next;
+			n++;
+		}
+	}
+	qsort((void *)devs_sorted, available, sizeof(struct hid_device_info *), compare_hid_device_info_ptr);
+
+	int count = 0;
+	for (i = 0; i < available; i++) {
+		cur_dev = devs_sorted[i];
+
+		if (strstr(cur_dev->path, "&col01#") != NULL) {
+			// Found matching serial number?
+			if (wcsicmp(cur_dev->serial_number, serial_number) == 0) {
+				// Call monitor event handler
+				PSMoveAPI::on_monitor_event(connect ? EVENT_DEVICE_ADDED : EVENT_DEVICE_REMOVED, EVENT_DEVICE_TYPE_BLUETOOTH, cur_dev->path, cur_dev->serial_number, user_data);
+			}
+		}
+		else {
+			count--;
+		}
+
+		count++;
+	}
+
+	free(devs_sorted);
+
+	// free HID device enumerations
+	for (i = 0; i < NUM_PSMOVE_PIDS; i++) {
+		devs = move_hid_devices[i];
+		if (devs) {
+			hid_free_enumeration(devs);
+		}
+	}
+}
+
+/** Notification window message handler **/
+LRESULT CALLBACK DeviceNotificationWindow::wndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+
+		// Device change notification?
+	case WM_DEVICECHANGE:
+		// Custom event?
+		if (wParam == DBT_CUSTOMEVENT)
+		{
+			DEV_BROADCAST_HDR *data = (DEV_BROADCAST_HDR*)lParam;
+			if (data != NULL && data->dbch_devicetype == DBT_DEVTYP_HANDLE)
+			{
+				// Bluetooth HCI event?
+				DEV_BROADCAST_HANDLE *handleData = (DEV_BROADCAST_HANDLE*)lParam;
+				if (IsEqualGUID(handleData->dbch_eventguid, GUID_BLUETOOTH_HCI_EVENT))
+				{
+					BTH_HCI_EVENT_INFO *hciData = (BTH_HCI_EVENT_INFO*)&handleData->dbch_data;
+					DeviceNotificationWindow *thiz = (DeviceNotificationWindow*)GetWindowLongPtr(hWnd, GWLP_USERDATA);
+					if (thiz != NULL)
+					{
+						// Handle event
+						thiz->handleBluetoothConnect(hciData->bthAddress, hciData->connected != 0);
+					}
+				}
+			}
+			return 0;
+		}
+		// Fall through...
+
+	default:
+		// Call default window message handler
+		return DefWindowProc(hWnd, uMsg, wParam, lParam);
+	}
+}
+#endif
 }; // end anonymous namespace
 
 ControllerGlue::ControllerGlue(int index, const std::string &serial)
@@ -178,14 +606,18 @@ PSMoveAPI::PSMoveAPI(EventReceiver *receiver, void *user_data)
         controllers.emplace_back(c);
     }
 
-#ifndef _WIN32
+#ifdef _WIN32
+	monitor = new DeviceNotificationWindow(this);
+#else
     monitor = moved_monitor_new(PSMoveAPI::on_monitor_event, this);
 #endif
 }
 
 PSMoveAPI::~PSMoveAPI()
 {
-#ifndef _WIN32
+#ifdef _WIN32
+	delete monitor;
+#else
     moved_monitor_free(monitor);
 #endif
 
